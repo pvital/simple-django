cf6f95e07a64e3466a297dd9b6a23e73d9309a5d
Paulo Vital <paulo.vital@ibm.com>
2025-04-02 00:40:25 +0200

    feat: Add view and control to simple_app
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- simple_app/urls.py
+++ simple_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /users/
     path("", views.index, name="index"),
+    # ex: /users/5/
+    path("<int:user_id>/", views.detail, name="detail"),
 ]
--- simple_app/views.py
+++ simple_app/views.py
@@ -1,5 +1,47 @@
-from django.http import HttpResponse
+import requests
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
+from .models import User
+
+
+def _request_users():
+    resp = requests.get("https://reqres.in/api/users").json()
+    return resp["data"]
+
+
+def _create_user(user_data):
+    user = User(
+        id=user_data.get("id"),
+        email=user_data.get("email"),
+        first_name=user_data.get("first_name"),
+        last_name=user_data.get("last_name"),
+        avatar=user_data.get("avatar"),
+    )
+    user.save()
+
+
+def _create_users():
+    for user in _request_users():
+        _create_user(user)
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Users index.")
+    users_list = User.objects.all()
+    if len(users_list) == 0:
+        try:
+            _create_users()
+        except Exception:
+            raise Http404("No users found.")
+        else:
+            users_list = User.objects.all()
+
+    context = {
+        "users_list": users_list,
+    }
+    return render(request, "users/index.html", context)
+
+
+def detail(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, "users/detail.html", {"user": user})
