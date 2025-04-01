8d09438d6eac1a84581194c06d59286ea738cdea
Paulo Vital <paulo.vital@ibm.com>
2025-04-01 23:18:48 +0200

    feat: Add User model to simple_app
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "simple_app",
+    "simple_app.apps.SimpleAppConfig",
 ]
 
 MIDDLEWARE = [
--- simple_app/models.py
+++ simple_app/models.py
@@ -1,3 +1,13 @@
+# Assisted by watsonx Code Assistant
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    avatar = models.URLField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
